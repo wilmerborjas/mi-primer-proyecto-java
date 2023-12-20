fcdc013286c0b388959f0bda396625bd4990b82c
wilmer Borjas <wilmer.aprendiendo.programacion@gmail.com>
2023-12-20 12:29:48 +0100

    aprendiendo a manejar atributos

---
+++ src/main/java/com/wbg/demo/Persona.java
@@ -0,0 +1,15 @@
+package com.wbg.demo;
+
+public class Persona {
+  
+	private String nombre;
+	private int edad;
+	private double altura;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/wbg/demo/Producto.java
@@ -0,0 +1,14 @@
+package com.wbg.demo;
+
+public class Producto {
+
+	
+	private String nombre;
+	private double valor;
+	private String capacidad;
+	private double tamanio;
+	private String color;
+	
+	
+
+}
